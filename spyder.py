1bf202b44ab1bfee974e2ef4150e8a33374818e4
spenceypantsy <142095957+spenceypantsy@users.noreply.github.com>
2023-09-30 19:56:54 +0800

    Add files via upload

---
+++ spyder.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 30 19:50:34 2023
+
+@author: script.pc #93
+"""
+
+# For loop to print integers between 1 to 10.
+for i in range (1, 11):
+    print(i)
