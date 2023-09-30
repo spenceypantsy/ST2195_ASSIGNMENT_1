7fc473113343a0749578281dbcc341ba0f65f5f8
spenceypantsy <142095957+spenceypantsy@users.noreply.github.com>
2023-09-30 19:44:34 +0800

    Add files via upload

---
+++ r_studio.R
@@ -0,0 +1,9 @@
+# Let i be integers between 1 to 10. 1 is initial condition, 10 is end condition.
+for (i in 1:10){
+  
+  # Concatenate and print with enter between each integer
+  cat(i, "\n")
+}
+
+
+
